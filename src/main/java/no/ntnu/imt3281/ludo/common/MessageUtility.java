d78afea76e08843db8bade177490c7cc505cfae0
Per-Morten Straume <p.m.straume@outlook.com>
2018-11-16 02:17:50 +0100

    Added class for message utility.
    Added a class for different message utility stuff. It will mostly be a
    holding place for JSON utility stuff, like adding an error code to the
    error array.

---
+++ src/main/java/no/ntnu/imt3281/ludo/common/MessageUtility.java
@@ -0,0 +1,17 @@
+package no.ntnu.imt3281.ludo.common;
+
+import no.ntnu.imt3281.ludo.api.Error;
+import no.ntnu.imt3281.ludo.api.FieldNames;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MessageUtility {
+    public static void addErrorToArray(JSONArray errors, int requestID, Error code) {
+        var error = new JSONObject();
+        error.put(FieldNames.ID, requestID);
+        var codes = new JSONArray();
+        codes.put(code.toInt());
+        error.put(FieldNames.CODE, codes);
+        errors.put(error);
+    }
+}
