c2887836d4a91a34492375c660fdcace73d3845e
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-11-17 16:18:55 +0100

    Implement rest of UserManger functionality
    UserManager for user interation should now be finished.

--- src/main/java/no/ntnu/imt3281/ludo/server/User.java
+++ src/main/java/no/ntnu/imt3281/ludo/server/User.java
@@ -2,7 +2,7 @@ package no.ntnu.imt3281.ludo.server;
 
 /*
  * POD for containing user data, all fields are public by intention.
- */
+  */
 public class User {
     @Deprecated
     private User() {
@@ -12,45 +12,45 @@ public class User {
     public User(int id, String username, String avatarURI) {
         this.id = id;
         this.username = username;
-        this.avatarURI = avatarURI;
+        this.avatarURI = (avatarURI != null) ? avatarURI : "";
     }
 
     public User(int id, String username, String avatarURI, String email, String salt, String token, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
-        this.avatarURI = avatarURI;
+        this.avatarURI = (avatarURI != null) ? avatarURI : "";
         this.salt = salt;
         this.token = token;
         this.password = password;
     }
 
-    public int id;
-    public String username;
-    public String avatarURI;
+    public int id = -1;
+    public String username = "";
+    public String avatarURI = "";
 
     /**
      * The email of the user, this will be null except when getExtendedInformation has been called.
      * Should not be exposed to the outside world!
      */
-    public String email;
+    public String email = "";
 
 
     /**
      * The salt used to hash the password, this will be null except when getExtendedInformation has been called.
      * Should not be exposed to the outside world!
      */
-    public String salt;
+    public String salt = "";
 
     /**
      * The authentication token of the user, this will be null except when getExtendedInformation has been called.
      * Should not be exposed to the outside world!
      */
-    public String token;
+    public String token = "";
 
     /**
      * The hashed password of the user, this will be null except when getExtendedInformation has been called.
      * Should not be exposed to the outside world!
      */
-    public String password;
+    public String password = "";
 }
--- src/main/java/no/ntnu/imt3281/ludo/server/UserManager.java
+++ src/main/java/no/ntnu/imt3281/ludo/server/UserManager.java
@@ -15,63 +15,90 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
 /**
- * Responsible for responding to user related messages
+ * Responsible for parsing and executing the incoming requests related to users & friends.
+ * Note: Class is not thread-safe as it communicates with the Database, which is not thread safe.
+ * Note: Neither validation nor authorization is done in this class,
+ *       it expects that all incoming requests have been authorized,
+ *       and that all are valid JSON objects according to the API specification.
  */
 public class UserManager {
-    Database mDB;
+    private Database mDB;
 
+    /**
+     * Creates a new UserManager with the given Database.
+     * @param db the database the UserManager should use.
+     */
     UserManager(Database db) {
         mDB = db;
     }
 
+    /**
+     * Parses and executes the given create user requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the create user requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void createUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        for (int i = 0; i < requests.length(); i++) {
-            var request = requests.getJSONObject(i);
-            var requestID = request.getInt(FieldNames.ID);
+        MessageUtility.each(requests, (requestID, request) -> {
             var username = request.getString(FieldNames.USERNAME);
             var email = request.getString(FieldNames.EMAIL);
 
             var password = request.getString(FieldNames.PASSWORD);
 
-            // Think: Really want to create the generalized exception.
             try {
                 var salt = randomGenerateSalt();
                 var hashedPWD = hashPassword(password, salt);
                 var userID = mDB.createUser(username, email, hashedPWD, salt);
                 var success = new JSONObject();
-
-                success.put(FieldNames.ID, requestID);
                 success.put(FieldNames.USER_ID, userID);
-                successes.put(success);
+                MessageUtility.appendSuccess(successes, requestID, success);
 
             } catch (SQLException e) {
                 Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to create user");
             } catch (APIErrorException e) {
-                MessageUtility.addErrorToArray(errors, requestID, e.getError());
+                MessageUtility.appendError(errors, requestID, e.getError());
             }
-        }
+        });
     }
 
+    /**
+     * Parses and executes the given delete user requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the delete user requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void deleteUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        for (int i = 0; i < requests.length(); i++) {
-            var request = requests.getJSONObject(i);
-            var requestID = request.getInt(FieldNames.ID);
+        MessageUtility.each(requests, (requestID, request) -> {
             var userID = request.getInt(FieldNames.USER_ID);
             try {
                 mDB.deleteUser(userID);
-            } catch(SQLException e) {
+                MessageUtility.appendSuccess(successes, requestID, new JSONObject());
+            } catch (SQLException e) {
                 Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
             }
-            var success = new JSONObject();
-            success.put(FieldNames.ID, requestID);
-            successes.put(success);
-        }
+        });
     }
 
+    /**
+     * Parses and executes the given update user requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the update user requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void updateUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        for (int i = 0; i < requests.length(); i++) {
-            var request = requests.getJSONObject(i);
-            var requestID = request.getInt(FieldNames.ID);
+        MessageUtility.each(requests, (requestID, request) -> {
             try {
                 var salt = randomGenerateSalt();
                 mDB.updateUser(request.getInt(FieldNames.USER_ID),
@@ -81,44 +108,85 @@ public class UserManager {
                         request.getString(FieldNames.AVATAR_URI),
                         salt);
 
-                var success = new JSONObject();
-                success.put(FieldNames.ID, requestID);
-                successes.put(success);
-
-            } catch(SQLException e) {
+                MessageUtility.appendSuccess(successes, requestID, new JSONObject());
+            } catch (SQLException e) {
                 Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
-            } catch(APIErrorException e) {
-                MessageUtility.addErrorToArray(errors, requestID, e.getError());
+            } catch (APIErrorException e) {
+                MessageUtility.appendError(errors, requestID, e.getError());
             }
-        }
+        });
     }
 
+    /**
+     * Parses and executes the given update get user requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the get user requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void getUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        Logger.log(Logger.Level.DEBUG, "Get users request received");
-        for (int i = 0; i < requests.length(); i++) {
-            var request = requests.getJSONObject(i);
-            var requestID = request.getInt(FieldNames.ID);
+        MessageUtility.each(requests, (requestID, request) -> {
             var userID = request.getInt(FieldNames.USER_ID);
             try {
                 var user = mDB.getUserByID(userID);
                 if (user != null) {
                     var json = userToJSON(user);
-                    json.put(FieldNames.ID, requestID);
-                    successes.put(json);
+                    MessageUtility.appendSuccess(successes, requestID, json);
                 } else {
-                    MessageUtility.addErrorToArray(errors, requestID, Error.USER_ID_NOT_FOUND);
+                    MessageUtility.appendError(errors, requestID, Error.USER_ID_NOT_FOUND);
                 }
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
             }
-        }
-
+        });
     }
 
+    /**
+     * Parses and executes the given update get user range requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the get user range requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void getUserRange(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        Logger.log(Logger.Level.DEBUG, "Get user range request received");
+        MessageUtility.each(requests, (requestID, request) -> {
+            var pageIdx = request.getInt(FieldNames.PAGE_INDEX);
+            try {
+                var users = mDB.getUserPage(pageIdx);
+                var usersArray = new JSONArray();
+
+                for (var user : users) {
+                    usersArray.put(userToJSON(user));
+                }
+
+                var success = new JSONObject();
+                success.put(FieldNames.RANGE, usersArray);
+                MessageUtility.appendSuccess(successes, requestID, success);
+
+            } catch (SQLException e) {
+                Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
+            }
+        });
     }
 
+    /**
+     * Parses and executes the given login request.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     * Note: Due to how we need the socketIDs for the SocketManager to be mapped to userIDs,
+     *       we do not accept multiple logins, we will simply take the first in the request.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the log in request to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void logInUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
         var request = requests.getJSONObject(0);
         var email = request.getString(FieldNames.EMAIL);
@@ -130,47 +198,68 @@ public class UserManager {
 
             // This will only return null if the user cannot be found.
             if (user == null) {
-                MessageUtility.addErrorToArray(errors, requestID, Error.INVALID_USERNAME_OR_PASSWORD);
+                MessageUtility.appendError(errors, requestID, Error.INVALID_USERNAME_OR_PASSWORD);
                 return;
             }
 
             var requestPassword = request.getString(FieldNames.PASSWORD);
             if (!hashPassword(requestPassword, user.salt).equals(user.password)) {
-                MessageUtility.addErrorToArray(errors, requestID, Error.INVALID_USERNAME_OR_PASSWORD);
+                MessageUtility.appendError(errors, requestID, Error.INVALID_USERNAME_OR_PASSWORD);
                 return;
             }
-
-            // TODO: If an exception is thrown from the database because the token isn't valid we need to retry!
-            var token = generateToken();
-            mDB.setUserToken(user.id, token);
+            
+            String token = null;
+            boolean isValidToken = false;
+            while (!isValidToken) {
+                token = generateToken();
+                try {
+                    mDB.setUserToken(user.id, token);
+                    isValidToken = true;
+                } catch(NotUniqueTokenException e) {
+                    Logger.log(Logger.Level.WARN, "Generated a token that was not unique! Retrying");
+                }
+            }
 
             var success = new JSONObject();
-            success.put(FieldNames.ID, requestID);
             success.put(FieldNames.USER_ID, user.id);
             success.put(FieldNames.AUTH_TOKEN, token);
-            successes.put(success);
+            MessageUtility.appendSuccess(successes, requestID, success);
 
         } catch (SQLException e) {
             Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
         }
     }
 
+    /**
+     * Parses and executes the given logout user requests.
+     * Successes are appended to the success array.
+     * Errors are appended to the error array.
+     *
+     * @param ignored
+     * @param requests  JSONArray containing all the get user range requests to execute.
+     * @param successes JSONArray where the successes should be appended to.
+     * @param errors    JSONArray where the errors should be appended to.
+     */
     public void logOutUser(RequestType ignored, JSONArray requests, JSONArray successes, JSONArray errors) {
-        for (int i = 0; i < requests.length(); i++) {
-            var request = requests.getJSONObject(i);
-            var requestID = request.getInt(FieldNames.ID);
+        MessageUtility.each(requests, (requestID, request) -> {
             var userID = request.getInt(FieldNames.USER_ID);
             try {
                 mDB.setUserToken(userID, null);
-            } catch(SQLException e) {
+                MessageUtility.appendSuccess(successes, requestID, new JSONObject());
+            } catch (SQLException e) {
                 Logger.logException(Logger.Level.WARN, e, "Unexpected SQL Exception when trying to log in user");
             }
-            var success = new JSONObject();
-            success.put(FieldNames.ID, requestID);
-            successes.put(success);
-        }
+        });
     }
 
+    /**
+     * Checks if the user with the specified token is authorized to execute the request
+     * stored in request.
+     *
+     * @param request The request to check if user is authorized to execute.
+     * @param token The token belonging to the user trying to execute the request.
+     * @return True if the user is authorized, false otherwise (also in the case where an exception is thrown, but that shouldn't happen).
+     */
     public boolean isUserAuthorized(JSONObject request, String token) {
         if (JSONValidator.hasInt(FieldNames.USER_ID, request)) {
             try {
@@ -199,7 +288,7 @@ public class UserManager {
         var builder = new StringBuilder();
         // Restricting the values here a bit because we got issues when we put "illegal" characters such as \n into json.
         // However, this should still provide enough place for randomness.
-        random.ints(64, 32, 126).forEach(i -> builder.append((char)i));
+        random.ints(64, 32, 126).forEach(i -> builder.append((char) i));
 
         return builder.toString();
     }
@@ -220,9 +309,8 @@ public class UserManager {
         var json = new JSONObject();
         json.put(FieldNames.USER_ID, user.id);
         json.put(FieldNames.USERNAME, user.username);
-        json.put(FieldNames.AVATAR_URI, user.avatarURI);
+        json.put(FieldNames.AVATAR_URI, (user.avatarURI != null) ? user.avatarURI : "");
 
         return json;
     }
-
 }
