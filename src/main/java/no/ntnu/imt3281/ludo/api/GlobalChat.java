6eae4ef08e5b9e82c92ab24b961b63d2c148ed36
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-11-20 01:32:33 +0100

    new GlobalChat.java

---
+++ src/main/java/no/ntnu/imt3281/ludo/api/GlobalChat.java
@@ -0,0 +1,12 @@
+package no.ntnu.imt3281.ludo.api;
+
+/**
+ * Contains metainformation about the single global chat
+ */
+public class GlobalChat {
+
+    /**
+     * Global chat should always have id = 0
+     */
+    public static final int id = 0;
+}
